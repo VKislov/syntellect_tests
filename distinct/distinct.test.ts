b139bb8c9810e94eaeddb9fc11da1c78947315e6
VKislov <wkislov0@gmail.com>
2024-11-04 22:07:55 +0500

    test(syntellect): fix test desc and name

--- distinct/distinct.test.ts
+++ distinct/distinct.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, test } from '@jest/globals'
 import { distinct } from './distinct' 
 
-describe('calcHandshakesForTenPerson', () => {
-    test('calculate number of handshakes for 10 person in 1 room', () => {
+describe('distinct', () => {
+    test('use distinct to string with repetetive creature names', () => {
         expect(distinct('cat,dog,human,cat,human,dog,cow,cow,owl')).toEqual('cat,dog,human,cow,owl')
     })
 })
